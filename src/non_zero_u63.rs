e34bdc7b03941d3753df9fa4aedd0e6fff6822b9
Brooks Prumo <brooks@prumo.org>
2022-05-02 07:58:58 -0500

    :pencil: Document NonZeroU63

--- src/non_zero_u63.rs
+++ src/non_zero_u63.rs
@@ -1,3 +1,4 @@
+/// A wrapper type for a u64 that is both non-zero and less-than 2^63.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[repr(transparent)]
 pub struct NonZeroU63(u64);
@@ -7,6 +8,11 @@ impl NonZeroU63 {
     pub const MIN: u64 = 1;
     pub const MAX: u64 = (1 << Self::BITS) - 1;
 
+    /// Make a new NonZeroU63, without any safety checks
+    ///
+    /// # Safety
+    ///
+    /// The caller must guarantee the input value is in range [1, 2^63 - 1].
     #[must_use]
     #[inline]
     pub const unsafe fn new_unchecked(n: u64) -> Self {
@@ -14,6 +20,9 @@ impl NonZeroU63 {
         Self(n)
     }
 
+    /// Make a new NonZeroU63
+    ///
+    /// The input value must be in range [1, 2^63 - 1], otherwise `None` will be returned.
     #[must_use]
     #[inline]
     pub const fn new(n: u64) -> Option<Self> {
